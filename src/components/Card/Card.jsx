e5b463d233792c85b0f1e9cbafae20a02af39cb9
MZ-87 <zsmile@mail.ru>
2022-11-24 11:07:15 +0300

    solved mistake in json, updated component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,13 +7,13 @@ function Card(props) {
             <img src={props.img} alt={props.id} className="img" />
             <h2 className={style.id}>{props.id}</h2>
             <h3 className={style.quote}>{props.quote}</h3>
-            <div className={style.alterego}><span>Альтерэго:</span> {props.alterego}</div>
-            <div className={style.universe}><span>Вселенная:</span> {props.universe}</div>
-            <div className="occupation item_info"><span>Род занятий:</span> {props.occupation}</div>
-            <div className="ally item_info"><span>Союзники:</span> {props.ally}</div>
-            <div className="enemy item_info"><span>Враги:</span> {props.enemy}</div>
-            <div className="powers item_info"><span>Суперспособности:</span> {props.powers}</div>
-            <div className="info item_info"><span>Подробнее:</span> {props.info}</div>
+            <div className={style.item_info}><span>Альтерэго:</span> {props.alterego}</div>
+            <div className={style.item_info}><span>Вселенная:</span> {props.universe}</div>
+            <div className={style.item_info}><span>Род занятий:</span> {props.occupation}</div>
+            <div className={style.item_info}><span>Союзники:</span> {props.ally}</div>
+            <div className={style.item_info}><span>Враги:</span> {props.enemy}</div>
+            <div className={style.item_info}><span>Суперспособности:</span> {props.powers}</div>
+            <div className={style.item_info}><span>Подробнее:</span> {props.info}</div>
         </div>
 
     );
